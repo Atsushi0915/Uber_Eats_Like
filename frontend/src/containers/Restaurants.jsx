a25ae74479646e70ef723036ec19ec6ec2c9fc52
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-05-24 22:26:54 +0900

    5-6 レストラン一覧ページを完成させる

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,16 +1,35 @@
-import React, { Fragment, useEffect, useReducer } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
+// --- ここから追加 ---
+import { Link } from "react-router-dom";
+// --- ここまで追加 ---
 
-import { fetchRestaurants } from '../apis/restaurants';  //apis
+// --- ここから追加 ---
+// components
+import Skeleton from '@material-ui/lab/Skeleton';
+// --- ここまで追加 ---
 
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
+
+// reducers
 import {
   initialState,
   restaurantsActionTyps,
   restaurantsReducer,
 } from '../reducers/restaurants';
 
-import MainLogo from '../images/logo.png';                //images
+// --- ここから追加 ---
+// constants
+import { REQUEST_STATE } from '../constants';
+// --- ここまで追加 ---
+
+// images
+import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+// --- ここから追加 ---
+import RestaurantImage from '../images/restaurant-image.jpg';
+// --- ここまで追加 ---
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -24,11 +43,39 @@ const MainLogoImage = styled.img`
 
 const MainCoverImageWrapper = styled.div`
   text-align: center;
-`
+`;
 
 const MainCover = styled.img`
   height: 600px;
 `;
+
+// --- ここから追加 ---
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+
+const RestaurantsContentWrapper = styled.div`
+  width: 250px;
+  height: 100px;
+  padding: 48px;
+`;
+
+const RestaurantsImageNode = styled.img`
+  width: 100%;
+`;
+
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
+// --- ここまで追加 ---
 
 export const Restaurants = () => {
   const [state, dispatch] = useReducer(restaurantsReducer, initialState);
@@ -46,7 +93,6 @@ export const Restaurants = () => {
       )
   }, [])
 
-
   return (
     <Fragment>
       <HeaderWrapper>
@@ -55,12 +101,28 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
-      {
-        state.restaurantsList.map(restaurant =>
-          <div>
-            {restaurant.name}
-          </div>)
-      }
+      // --- ここから追加 ---
+      <RestaurantsContentsList>
+        {
+          state.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+            </Fragment>
+            :
+            state.restaurantsList.map((item, index) =>
+              <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+                <RestaurantsContentWrapper>
+                  <RestaurantsImageNode src={RestaurantImage} />
+                  <MainText>{item.name}</MainText>
+                  <SubText>{`配送料：${item.fee}円 ${item.time_required}分`}</SubText>
+                </RestaurantsContentWrapper>
+              </Link>
+            )
+        }
+      </RestaurantsContentsList>
+      // --- ここまで追加 ---
     </Fragment>
   )
 }
